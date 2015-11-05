3ae055b7178b49710b80cbeced0f35c411ec846f
Carlos Castillo <sir.ragnar@gmail.com>
2015-11-05 18:44:38 +0100

    Added tests for User

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,40 @@
 package org.craftedsw.tripservicekata.user;
 
-public class UserTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import org.craftedsw.tripservicekata.trip.Trip;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+	
+	private User user;
+	
+	@Before
+	public void setUp(){
+		user = new User();
+	}
+	
+	@Test
+	public void new_user_has_not_null_friend_list(){
+		assertNotNull(user.getFriends());
+	}
+	
+	@Test
+	public void new_user_has_not_null_trips(){
+		assertNotNull(user.trips());
+	}
+	
+	@Test
+	public void addFriend_adds_one_friend_to_friend_list(){
+		user.addFriend(new User());
+		assertEquals(1,user.getFriends().size());
+	}
+	
+	@Test
+	public void addTrip_adds_one_trip_to_trip_list(){
+		user.addTrip(new Trip());
+		assertEquals(1,user.trips().size());
+	}
 }
