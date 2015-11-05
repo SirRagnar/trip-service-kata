f2972d521a252e6e1f7929b0c01af2c7cc60c4e8
Carlos Castillo <sir.ragnar@gmail.com>
2015-11-05 21:23:42 +0100

    Added a first green test over TripService.
    Added a test testing that a user different from logged user with no
    friends has a not null trip list

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -29,7 +29,7 @@ public class TripService {
 		}
 	}
 
-	private User getLoggedUser() {
+	public User getLoggedUser() {
 		return UserSession.getInstance().getLoggedUser();
 	}
 
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,30 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.craftedsw.tripservicekata.user.User;
+
 public class TripServiceTest {
-	
+	private TripService tripService;
+
+	private User loggedUser;
+
+	@Before
+	public void setUp() {
+		tripService = mock(TripService.class);
+		loggedUser = new User();
+	}
+
+	@Test
+	public void user_with_no_friends_has_no_trips() {
+		when(tripService.getLoggedUser()).thenReturn(loggedUser);
+		assertNotNull(tripService.getTripsByUser(new User()));
+	}
+
 }
