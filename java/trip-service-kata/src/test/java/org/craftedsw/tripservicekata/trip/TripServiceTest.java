229e9f9be654c700d5786fe8c6b14cc2d4cf3011
Carlos Castillo <sir.ragnar@gmail.com>
2015-11-05 21:26:21 +0100

    Improved no friends test testing that size of trip list is zero

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -3,10 +3,13 @@ package org.craftedsw.tripservicekata.trip;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.craftedsw.tripservicekata.user.User;
 
@@ -24,7 +27,10 @@ public class TripServiceTest {
 	@Test
 	public void user_with_no_friends_has_no_trips() {
 		when(tripService.getLoggedUser()).thenReturn(loggedUser);
-		assertNotNull(tripService.getTripsByUser(new User()));
+		User otherUser = new User();
+		List<Trip> trips = tripService.getTripsByUser(otherUser);
+		assertNotNull(trips);
+		assertEquals(0,trips.size());
 	}
 
 }
