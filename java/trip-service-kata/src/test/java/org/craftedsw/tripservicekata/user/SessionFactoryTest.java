443f59b1fcc219b26712a2ca9e56af18e6a9e336
Carlos Castillo <sir.ragnar@gmail.com>
2015-11-05 19:54:39 +0100

    Refactor SessionFactory test in order to use the static function instead of instance function

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/SessionFactoryTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/SessionFactoryTest.java
@@ -7,16 +7,14 @@ import org.junit.Test;
 
 public class SessionFactoryTest {
 
-	private SessionFactory sessionFactory;
-
 	@Before
 	public void setUp() {
-		sessionFactory = new SessionFactory();
+
 	}
 
 	@Test
 	public void getUserSession_returns_a_not_null_UserSession() {
-		assertNotNull(sessionFactory.getUserSession());
+		assertNotNull(SessionFactory.getUserSession());
 	}
 
 }
