37e78d7656a168cc328a94de56075f3f355758b9
Carlos Castillo <sir.ragnar@gmail.com>
2015-11-06 20:00:49 +0100

    Created empty TripDAOWrapper with and empty TripDAOWrapperTest

---
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripDAOWrapperTest.java
@@ -0,0 +1,14 @@
+package org.craftedsw.tripservicekata.trip;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TripDAOWrapperTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+}
