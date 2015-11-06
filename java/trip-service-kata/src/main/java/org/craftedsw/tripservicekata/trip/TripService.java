c4a99bdc0f5743b25925c32a466f1a81d965da89
Carlos Castillo <sir.ragnar@gmail.com>
2015-11-06 20:11:20 +0100

    Added a userSessionWrapper field in TripService

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -6,11 +6,14 @@ import java.util.List;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
+import org.craftedsw.tripservicekata.user.UserSessionWrapper;
 
 public class TripService {
-	
+
 	public TripDAOWrapper tripDAOWrapper = new TripDAOWrapper();
-	
+
+	public UserSessionWrapper userSessionWrapper = new UserSessionWrapper();
+
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
 		User loggedUser = getLoggedUser();
